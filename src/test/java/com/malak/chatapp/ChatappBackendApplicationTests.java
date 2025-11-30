cde372af78991d5efaf81ba6b43f8acf694a6538
Malak-Essam <malakmeke100@gmail.com>
2025-11-30 02:24:17 +0200

    init commit

---
+++ src/test/java/com/malak/chatapp/ChatappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malak.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
