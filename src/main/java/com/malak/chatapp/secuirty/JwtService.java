14d6b7ad872f13b3ced4b1344b861a9a8729b5c9
Malak-Essam <malakmeke100@gmail.com>
2025-11-30 02:52:51 +0200

    created jwt service

---
+++ src/main/java/com/malak/chatapp/secuirty/JwtService.java
@@ -0,0 +1,66 @@
+package com.malak.chatapp.secuirty;
+
+import java.util.Date;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.malak.chatapp.domain.Role;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+public class JwtService {
+	@Value("{jwt.secret}")
+	private String secretKey;
+	
+	@Value("{jwt.expiration}")
+	private long expiration;
+	
+	private SecretKey getSigningKey() {
+		return Keys.hmacShaKeyFor(secretKey.getBytes());
+	}
+	private String generateToken(String username, Role role) {
+		return Jwts.builder()
+				.subject(username)
+				.claim("role", role)
+				.issuedAt(new Date())
+				.expiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(getSigningKey())
+				.compact();
+	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser()
+				.verifyWith(getSigningKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+	}
+	
+	private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(extractAllClaims(token));
+    }
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	public String extractRole(String token) {
+		return extractAllClaims(token).get("role", String.class);
+	}
+	public boolean isTokenValid(String token, String username) {
+        return username.equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+	
+}
