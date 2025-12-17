78fb8bb4916500b6741c9240f4683b9a94ddaf2c
Malak-Essam <malakmeke100@gmail.com>
2025-12-17 04:51:12 +0200

    added frontend to cros

--- src/main/java/com/malak/chatapp/config/SecurityConfig.java
+++ src/main/java/com/malak/chatapp/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
 	@Bean
     public CorsConfigurationSource corsConfigurationSource() {
     	CorsConfiguration config = new CorsConfiguration();
-    	config.setAllowedOriginPatterns(List.of("*"));
+    	config.setAllowedOriginPatterns(List.of("https://chat-frontend-dusky-five.vercel.app","*"));
     	config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
