e791226679f5f2d3c83e822aa82f34f09bdc2cb0
Malak-Essam <malakmeke100@gmail.com>
2025-12-17 03:43:12 +0200

    added get user by id to user controller

--- src/main/java/com/malak/chatapp/controller/UserController.java
+++ src/main/java/com/malak/chatapp/controller/UserController.java
@@ -3,7 +3,9 @@ package com.malak.chatapp.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +14,13 @@ import com.malak.chatapp.dto.UserDto;
 import com.malak.chatapp.service.UserService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Validated
 @Tag(name = "User")
 public class UserController {
     
@@ -27,5 +31,10 @@ public class UserController {
         List<UserDto> users = userService.findAllUsers();
         
         return ResponseEntity.ok(ApiResponse.success(users, "Users retrieved successfully"));
+    }
+    @GetMapping("/{userId}")
+    public ResponseEntity<ApiResponse<UserDto>> getUser(@PathVariable @NotNull Long userId) {
+    	
+        return ResponseEntity.ok(ApiResponse.success(userService.getUserById(userId), "Users retrieved successfully"));
     }
 }
